6cd5bee5a4d1a7945a903ed172eb9f12320699ae
Ficram Manifur Farissa <ficramm@gmail.com>
2026-08-26 15:13:21 +0700

    Update test.ino

--- firmware/esp32/sensor/hx711/test.ino
+++ firmware/esp32/sensor/hx711/test.ino
@@ -1,48 +1,58 @@
+#include <Arduino.h>
 #include "HX711.h"
 
 // ==============================
-// PIN HX711
+// ESP32-S3 N16R8 + HX711
 // ==============================
-#define HX711_DOUT 32
-#define HX711_SCK  33
+#define LOADCELL_DOUT_PIN 4
+#define LOADCELL_SCK_PIN  5
 
 HX711 scale;
 
-// ==============================
-// KALIBRASI
-// ==============================
-// Ganti nilai ini setelah proses kalibrasi
-float calibration_factor = 454.542;
+// Calibration factor sementara
+float calibration_factor = 459.542;
+
+// Batas noise yang dianggap 0 gram
+const float ZERO_THRESHOLD = 1.0;
 
 void setup() {
   Serial.begin(115200);
-  delay(1000);
+  delay(2000);
 
+  Serial.println();
   Serial.println("================================");
-  Serial.println("   LOAD CELL + HX711 + ESP32-S3");
+  Serial.println("ESP32-S3 N16R8 + HX711");
   Serial.println("================================");
 
-  scale.begin(HX711_DOUT, HX711_SCK);
+  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
 
+  // Gunakan calibration factor
   scale.set_scale(calibration_factor);
-  scale.tare();  // Set kondisi awal = 0 gram
+
+  // Pastikan load cell kosong
+  Serial.println("Pastikan load cell TANPA BEBAN...");
+  delay(2000);
+
+  // Tare otomatis
+  scale.tare();
 
   Serial.println("Tare selesai.");
-  Serial.println("Letakkan beban...");
+  Serial.println("Berat awal = 0 gram");
+  Serial.println("================================");
 }
 
 void loop() {
-  if (scale.is_ready()) {
 
-    float weight = scale.get_units(10);
+  float berat = scale.get_units(10);
 
-    Serial.print("Berat: ");
-    Serial.print(weight, 2);
-    Serial.println(" gram");
-
-  } else {
-    Serial.println("HX711 tidak terdeteksi!");
+  // Hilangkan noise kecil di sekitar titik nol
+  if (abs(berat) < ZERO_THRESHOLD) {
+    berat = 0.0;
   }
+
+  Serial.print("Berat: ");
+  Serial.print(berat, 2);
+  Serial.println(" gram");
 
   delay(500);
 }
